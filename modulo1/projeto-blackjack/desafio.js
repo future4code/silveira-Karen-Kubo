0a786cb75dd812b73654e809068a3e87ea40bae1
Karen Kubo <karennckubo@gmail.com>
2022-02-25 13:03:56 -0300

    projeto2-semana3

---
+++ modulo1/projeto-blackjack/desafio.js
@@ -0,0 +1,29 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+// function sorteiaUser() {
+// const cartas = comprarCarta();
+// let cartaUser = [cartas.texto, cartas.texto];
+// return cartaUser;
+// }
+
+// function sorteiaComp() { 
+//    const cartas = comprarCarta();
+//    let cartaComp = [cartas.texto, cartas.texto];
+//    return cartaComp
+// }
+
+// function sorteia2A() {
+//    if (cartaUser === ["A", "A"] || cartaComp === ["A", "A"]) {
+//       return cartas
+//    }};
