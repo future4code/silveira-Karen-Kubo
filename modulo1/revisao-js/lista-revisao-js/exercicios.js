045c411e07165ca8c17ddd7d2f026c8eb9ba4a07
Karen Kubo <karennckubo@gmail.com>
2022-03-07 12:15:31 -0300

    revisao-js

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -103,12 +103,13 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-    if (ladoA === ladoB === ladoC) {
+    if (ladoA === ladoB && ladoB === ladoC) {
         return `Equilátero`
-    } else if (ladoA == ladoB || ladoB == ladoC || ladoA == ladoC) {
-        return `Isósceles`
+    } else if (ladoA !== ladoB && ladoB !== ladoC && ladoA !== ladoC) {
+        return `Escaleno`
     } else {
-        return `Escaleno`};
+        return `Isósceles`
+    }
 }
 
 // EXERCÍCIO 10
