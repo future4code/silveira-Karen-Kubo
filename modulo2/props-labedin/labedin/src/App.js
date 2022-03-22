2beb4c73b600069a2ab8a1e60c43d0afb2c9568a
Karen Kubo <karennckubo@gmail.com>
2022-03-22 13:27:19 -0300

    labedin

---
+++ modulo2/props-labedin/labedin/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import CardPequeno from './components/CardPequeno/CardPequeno';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import ImagemPessoal from './img/img-pessoal.jpeg';
+import ImgPhone from './img/phone-call.png';
+import ImgEmail from './img/email.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem={ImagemPessoal} 
+          nome="Karen Kubo" 
+          descricao="Oi, eu sou a Karen, tenho 22 anos e sou aluna da turma Silveira da Labenu."
+        />
+        
+        <ImagemButton 
+          imagem="https://cdn-icons-png.flaticon.com/512/929/929750.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div>
+        <CardPequeno 
+        imagememail={ImgEmail}
+        email="karennckubo@gmail.com"
+        imagemcel={ImgPhone}
+        celular="19 99438-0962"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+        
+        <CardGrande 
+          imagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/2449px-NASA_logo.svg.png" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+
+
+    </div>
+
+  );
+}
+
+export default App;
