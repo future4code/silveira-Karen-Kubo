aa0ab3f87ee6ce6ac81d6e533b54ed9e355aca9b
Karen Kubo <karennckubo@gmail.com>
2022-03-25 19:41:49 -0300

    acrescentado css

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -4,14 +4,18 @@ import { SecaoComentario } from './components/SecaoComentario';
 
 
 const GlobalStyle = createGlobalStyle`
-margin:0;
-padding: 0;
-box-sizing: border-box;
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+
 `
 const MainContainer = styled.div`
-height: 100vh;
-width: 80vw;
-margin: auto;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+
+  font-family: "Times New Roman";
+  background-image: linear-gradient(to top, #d5d4d0 0%, #d5d4d0 1%, #eeeeec 31%, #efeeec 75%, #e9e9e7 100%);
 
 `
 
@@ -20,13 +24,14 @@ margin: auto;
 export default class App extends React.Component {
     render() {
       return (
+      
       <MainContainer>
         <GlobalStyle/>
 
           <SecaoComentario>
 
           </SecaoComentario>
- 
+        
       </MainContainer>
       )
     }
