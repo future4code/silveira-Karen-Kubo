165a5e499831a06b206602ead575d1ac103f900b
Karen Kubo <karennckubo@gmail.com>
2022-06-28 08:43:22 -0300

    alteracoes

--- modulo5/introducao-autenticacao/src/data/BaseDatabase.ts
+++ modulo5/introducao-autenticacao/src/data/BaseDatabase.ts
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export class BaseDatabase {
-  protected static connection = knex({
-    client: "mysql",
-    connection: {
-      host: process.env.DB_HOST,
-      port: 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_SCHEMA
-    }
-  })
-}
+export const userTableName = "User";
+
+export const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+  },
+});
+
--- modulo5/introducao-autenticacao/src/model/StudentModel.ts
+++
@@ -1,8 +0,0 @@
-import { BaseDatabase } from "../data/baseDatabase";
-
-export class StudentModel {
-    constructor(private id: string){}
-    public getId(): string {
-        return this.id;
-    }    
-}
