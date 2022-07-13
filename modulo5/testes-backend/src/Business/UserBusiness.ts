1354ef3aab8bfa5ed83307b78ac761e1a9087640
Karen Kubo <karennckubo@gmail.com>
2022-07-13 16:54:53 -0300

    correção

--- modulo5/testes-backend/src/Business/UserBusiness.ts
+++ modulo5/testes-backend/src/Business/UserBusiness.ts
@@ -1,9 +1,11 @@
+import { TokenGenerator } from './../Services/tokenGenerator';
 import { CustomError } from '../Errors/CustomError';
 import { UserData, User } from './../Data/User';
 
 export class UserBusiness {
     constructor(
-        private userData:UserData
+        private userData:UserData,
+        private tokenGenerator:TokenGenerator
     ){};
 
     getUserProfile = async (id:string) => {
@@ -20,6 +22,7 @@ export class UserBusiness {
         return user;
     }
 
+
     getAllUsers = async (id:string) => {
 
         if(!id) {
@@ -30,12 +33,30 @@ export class UserBusiness {
         if (!user) {
             throw new CustomError(404, "Usuário não encontrado");
         };
-        
+
         if(user.getRole() !== "ADMIN") {
             throw new CustomError(401, "Você não possui autorização para visualizar os usuários");
         };
         const users = await this.userData.getAllUsers();
 
         return users;
+    }
+
+    getYourOwnProfile = async (token:string) => {
+
+        if(!token) {
+            throw new CustomError(422, `Por favor, informe o token corretamente`);
+        }
+
+        const data = this.tokenGenerator.verify(token);
+        if (!data) {
+            throw new CustomError(404, "Usuário não encontrado");
+
+        }
+        
+        const user = await this.userData.getInfo(data.id);
+
+
+        return user;
     }
 }
--- modulo5/testes-backend/tests/UserBusiness.test.ts
+++ modulo5/testes-backend/tests/UserBusiness.test.ts
@@ -1,9 +1,12 @@
+import { TokenGenerator } from './../src/Services/tokenGenerator';
 import { UserData } from './../src/Data/User';
 import { UserBusiness } from './../src/Business/UserBusiness';
 import { UserDataMock, UserMockAdmin, UserMockNormal } from './mocks/UserMock';
+import { TokenMock } from './mocks/TokenMock';
 
 const userBusinessMock = new UserBusiness(
-    new UserDataMock() as UserData
+    new UserDataMock() as UserData,
+    new TokenMock() as TokenGenerator
 )
 
 const getUserById = jest.fn(
@@ -11,6 +14,9 @@ const getUserById = jest.fn(
 )
 const getAllUsers = jest.fn(
     (id: string) => userBusinessMock.getAllUsers(id)
+)
+const getYourOwnProfile = jest.fn(
+    (token: string) => userBusinessMock.getYourOwnProfile(token)
 )
 
 describe("getUserById", () => {
@@ -66,6 +72,36 @@ describe("getAllUsers", () => {
 
             expect(users).toEqual([UserMockAdmin, UserMockNormal]);
             expect(getUserById).toHaveBeenCalledWith("id_mock_admin")
+        } catch (error:any) {
+            expect(error.message).toBe(`Usuário não encontrado`);
+
+        }finally{
+            expect.assertions(2);
+        }
+    })
+})
+
+describe("getYourOwnProfile", () => {
+	// (a)
+    test("Erro se o usuário não for encontrado", async () => {
+        try {
+            const profile = await getYourOwnProfile("ABC");
+        } catch (error:any) {
+            expect(error.statusCode).toBe(404);
+            expect(error.message).toBe(`Usuário não encontrado`);
+
+        } finally {
+            expect.assertions(2);
+        }
+    })
+    
+	// (b)
+    test("Sucesso devolvendo o próprio perfil", async () => {
+        try {
+            const profile = await getYourOwnProfile("token_normal");
+
+            expect(profile).toEqual(UserMockNormal);
+            expect(getYourOwnProfile).toHaveBeenCalledWith("token_normal")
         } catch (error:any) {
             expect(error.message).toBe(`Usuário não encontrado`);
 
